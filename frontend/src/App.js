4a81bc48a83a59801097015251d4b7eaf19eb6b7
Nilesh <nilesh21098@yahoo.com>
2020-12-14 14:21:32 +0530

    added product screen and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,31 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">myShopp</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
-        </header>
-        <main>
-          <div>
-            <div className="row center">
-              {
-                 data.products.map(product => (
-                  <Product key={product._id} product={product} />
-                 ))
-              }
-            </div>
-          </div>
-        </main>
-        <footer className="row center">
-            All right reserved
-        </footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+          <header className="row">
+              <div>
+                  <a className="brand" href="/">myShopp</a>
+              </div>
+              <div>
+                  <a href="/cart">Cart</a>
+                  <a href="/signin">Sign In</a>
+              </div>
+          </header>
+          <main>
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" component={HomeScreen} exact />
+            
+          </main>
+          <footer className="row center">
+              All right reserved
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
