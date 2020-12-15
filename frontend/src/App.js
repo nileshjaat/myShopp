9d7b61967fc297c9049a18a37ef99421d3e41af7
Nilesh <nilesh21098@yahoo.com>
2020-12-15 10:31:24 +0530

    loaded data from backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-          <header className="row">
-              <div>
-                  <a className="brand" href="/">myShopp</a>
-              </div>
-              <div>
-                  <a href="/cart">Cart</a>
-                  <a href="/signin">Sign In</a>
-              </div>
-          </header>
-          <main>
-            <Route path="/product/:id" component={ProductScreen} />
-            <Route path="/" component={HomeScreen} exact />
-            
-          </main>
-          <footer className="row center">
-              All right reserved
-          </footer>
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              myShopp
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </main>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
